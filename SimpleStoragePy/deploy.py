276c106b47eed8d98587f14353bb2646ee055ddd
Eggyf <abdelfregel1711@gmail.com>
2023-12-05 10:30:53 -0500

    "install web3"

---
+++ SimpleStoragePy/deploy.py
@@ -0,0 +1,61 @@
+from solcx import compile_standard, install_solc
+import json
+import os
+from dotenv import load_dotenv
+from web3 import Web3
+
+load_dotenv()
+with open("./SimpleStorage.sol", "r") as file:
+    simple_storage_file = file.read()
+
+
+# Compilar
+install_solc("0.6.0")
+compiled_sol = compile_standard(
+    {
+        "language": "Solidity",
+        "sources": {"SimpleStorage.sol": {"content": simple_storage_file}},
+        "settings": {
+            "outputSelection": {
+                "*": {
+                    "*": ["abi", "metadata", "evm.bytecode", "evm.bytecode.sourceMap"]
+                }
+            }
+        },
+    },
+    solc_version="0.6.0",
+)
+
+
+with open("compiled_code.json", "w") as file:
+    json.dump(compiled_sol, file)
+
+bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
+    "bytecode"
+]["object"]
+abi = json.loads(
+    compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["metadata"]
+)["output"]["abi"]
+
+w3 = Web3(Web3.HTTPProvider("HTTP://127.0.0.1:7545"))
+chainid = 1337
+my_address = "0x21EA3aEB59Ea693a31a6E1771fe9e29Df3818515"
+private_key = os.getenv("PRIVATE_KEY")
+
+SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
+print(SimpleStorage)
+
+# nonce
+nonce = w3.eth.get_transaction_count(my_address)
+
+transaction = SimpleStorage.constructor().build_transaction(
+    {
+        "chainId": chainid,
+        "gasPrice": w3.eth.gas_price,
+        "from": my_address,
+        "nonce": nonce,
+    }
+)
+
+signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
+print(signed_txn)
